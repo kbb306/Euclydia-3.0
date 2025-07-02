502caf9028ebe300e32318dab2a0d0635b33f72e
kbb306 <kylerbbelshaw@gmail.com>
2025-07-02 13:07:14 -0600

    Muted DEBUG spam.

--- Translator.py
+++ Translator.py
@@ -92,18 +92,18 @@ class translator():
 
     def filein(self, file):
         readaloud = ggwavin()
-        print("[DEBUG] Reading:", file)
+        #print("[DEBUG] Reading:", file)
         compressed = readaloud.ggwave_from_file(file)
         hit = False
         for each in compressed:
-            print("[DEBUG] Got from file:", repr(each))
+            #print("[DEBUG] Got from file:", repr(each))
             hit = True
             if not each or not isinstance(each, str) or len(each) < 3:
-                print("[Skip] Invalid or empty signal:", repr(each))
+                #print("[Skip] Invalid or empty signal:", repr(each))
                 continue
             try:
                 phrase = self.middleman.decode(each)
-                print("Received text:", phrase)
+                #print("Received text:", phrase)
             except Exception as e:
                 print("[Decode Error]", e, "| Raw:", repr(each))
         if not hit:
