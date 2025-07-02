b2e47d0ae1e9fcf2eeb1e706b5979c347b61926d
kbb306 <kylerbbelshaw@gmail.com>
2025-07-02 10:41:58 -0600

    Adjusted comments

--- smaz_encode.py
+++ smaz_encode.py
@@ -4,14 +4,14 @@ from base91 import encode as b91_encode, decode as b91_decode
 class smaz_wrapper:
     @staticmethod
     def encode(phrase: str) -> str:
-        """Compress text and return a latin1-safe string."""
+        """Compress text and return a base91 string"""
         compressed = smaz.compress(phrase)
         encoded = b91_encode(compressed)
         return encoded
 
     @staticmethod
     def decode(codephrase: str) -> str:
-        """Decode a latin1-safe string to original text."""
+        """Decode a base91-smaz string to original text."""
         decoded = b91_decode(codephrase)
         decompressed = smaz.decompress(decoded)
         return decompressed
