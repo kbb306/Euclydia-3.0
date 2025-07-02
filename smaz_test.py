bbefb2a576e70e145998c98630c8115eab0c0777
kbb306 <kylerbbelshaw@gmail.com>
2025-07-02 00:31:08 -0600

    Added a test to see if compression is actually doing ANYTHING.

---
+++ smaz_test.py
@@ -0,0 +1,22 @@
+from smaz import compress, decompress
+
+def encode():
+   phrase = input("Enter <100 characters of text: ")
+   codephrase = compress(phrase)
+   print(codephrase)
+
+def decode():
+   codephrase = input("Enter compressed text: ")
+   phrase = decompress(codephrase)
+   print(phrase)
+
+def main():
+   while True:
+        selection = input("Encode or Decode (Or Quit)? ")
+        if selection.upper == "E":
+            encode()
+        elif selection.upper == "D":
+            decode()
+        elif selection.upper == "Q":
+            print("Goodbye!")
+            break
