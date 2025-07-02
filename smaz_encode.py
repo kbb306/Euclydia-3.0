52ce96e7e1c61900c84ccb628c07c086bfcff0a3
kbb306 <kylerbbelshaw@gmail.com>
2025-07-02 11:11:56 -0600

    Ensured smaz_wrapper.encode() recieves bytes.

--- smaz_encode.py
+++ smaz_encode.py
@@ -6,6 +6,8 @@ class smaz_wrapper:
     def encode(phrase: str) -> str:
         """Compress text and return a base91 string"""
         compressed = smaz.compress(phrase)
+        if isinstance(compressed, str):
+            compressed = compressed.encode("latin1")
         encoded = b91_encode(compressed)
         return encoded
 
